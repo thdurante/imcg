27a6b61d0bff4a2293fde1a6e740b8c03ad20fa1
Thiago Durante Pires <thdurante@gmail.com>
2018-05-27 15:40:11 -0300

    Implement and test PeopleController.

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,45 @@
+class PeopleController < ApplicationController
+  layout 'authentication'
+
+  before_action :validate_person_registration, only: :registration
+  before_action :set_person_by_cpf, only: %i(registration create_or_update_registration)
+
+  def index
+    @person_registration = PersonRegistration.new
+  end
+
+  def registration; end
+
+  def create_or_update_registration
+    @person.assign_attributes(person_params)
+    if @person.save
+      redirect_to root_path, notice: t('.notice')
+    else
+      render :registration
+    end
+  end
+
+  private
+
+  def validate_person_registration
+    @person_registration = PersonRegistration.new(person_registration_params)
+    render :index unless @person_registration.valid?
+  end
+
+  def set_person_by_cpf
+    cpf = @person_registration ? @person_registration.cpf : params[:person].try(:[], :cpf)
+    @person = Person.where(cpf: CPF.new(cpf).stripped).first_or_initialize
+  end
+
+  def person_registration_params
+    params.fetch(:person_registration, {}).permit(:cpf)
+  end
+
+  def person_params
+    params.require(:person).permit(
+      :id, :cpf, :rg, :name, :email, :occupation, :member,
+      address_attributes: %i(id street number complement neighborhood city state),
+      phones_attributes: %i(id kind number _destroy)
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root 'dashboards#index'
-  get 'dashboards/admin', to: 'dashboards#admin'
-  get 'addresses/cities', to: 'addresses#cities'
+  root 'people#index'
+
+  get  'addresses/cities',    to: 'addresses#cities'
+  get  'dashboards/admin',    to: 'dashboards#admin'
+  get  'dashboards/',         to: 'dashboards#index'
+  get  'people/registration', to: 'people#registration',                  as: :person_registration
+  post 'people/register',     to: 'people#create_or_update_registration', as: :register_person
 end
